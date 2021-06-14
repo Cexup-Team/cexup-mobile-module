7ce5a73671958541c9427c8bb615c4e7cc9b5d9a
Trian <triannurizkillah@gmail.com>
2021-06-14 16:52:33 +0700

    feat :init

---
+++ app/src/main/java/com/trian/MainActivity.java
@@ -0,0 +1,22 @@
+package com.trian;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+    /***
+     * ini adalah omen
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
